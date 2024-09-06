98fe0fad0bdaf54788ad87557ababe247a2b29c6
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-06 15:25:48 +0200

    fixed database communication

--- src/initdb.js
+++ src/initdb.js
@@ -1,32 +1,48 @@
 // THIS CODE MAY ONLY BE RUN ONCE
 // it just won't do anything the 2nd time
 
-const executeQuery = require("./dbconnection.js");
+const mysql = require("mysql");
 
 const DBPASSWORD = process.env.DBPASSWORD;
 const DBUSER = process.env.DBUSER;
 const DBNAME = process.env.DBNAME;
 const DBHOST = process.env.DBSERVERNAME;
 
-// try and create database
-executeQuery(`CREATE DATABASE ${DBNAME}`).catch(err => {
-    // don't throw error if database creation fails; this is normal
-    // upon program restart if database has already been created
-    if (err.code !== "ER_DB_CREATE_EXISTS") throw err;
-    else console.log(err);
+const con = mysql.createConnection({
+    host: DBHOST,
+    user: DBUSER,
+    password: DBPASSWORD
 });
+
+// try and create database
+// executeQuery(`CREATE DATABASE ${DBNAME}`).catch(err => {
+//     // don't throw error if database creation fails; this is normal
+//     // upon program restart if database has already been created
+//     if (err) throw err;
+// });
+con.query(`CREATE DATABASE ${DBNAME}`);
 
 // try and create table for userdata
 // line continuation ("\") so that the line is not interpreted with newline characters ("\n")
-executeQuery(`CREATE TABLE userdata (\
+// executeQuery(`CREATE TABLE userdata (\
+// id VARCHAR(36) NOT NULL PRIMARY KEY,\
+// name VARCHAR(30) NOT NULL,\
+// email VARCHAR(30) NOT NULL,\
+// password VARCHAR(50) NOT NULL,\
+// location_lat VARCHAR(50) NOT NULL,\
+// location_long VARCHAR(50) NOT NULL,\
+// reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+// )`).catch(err => {
+//     if (err) throw err;
+// });
+con.query(`USE journeymate`);
+
+con.query(`CREATE TABLE userdata (\
 id VARCHAR(36) NOT NULL PRIMARY KEY,\
 name VARCHAR(30) NOT NULL,\
 email VARCHAR(30) NOT NULL,\
 password VARCHAR(50) NOT NULL,\
 location_lat VARCHAR(50) NOT NULL,\
 location_long VARCHAR(50) NOT NULL,\
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP    
-)`).catch(err => {
-    if (err.code !== "ER_TABLE_EXISTS_ERROR") throw err;
-    else console.log(err);
-});
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)`);
