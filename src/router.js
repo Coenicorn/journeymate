e94d738b14a5938c3811c152e31741c2e22f25fe
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-06 13:25:52 +0200

    working on route requests

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 // specify app router to handle incoming requests from the client
-router.get("/signin", (request, response) => {
+router.post("/signup", (request, response) => {
     // DEBUG log the request
     console.log(request.body);
 
     response.status(200);
-    response.send("OK");
+    response.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("@dotenvx/dotenvx").config();
 const mysql = require("mysql");
+const path = require("path");
 
 const app = express();
 
+// get environment variables
 const DBPASSWORD = process.env.DBPASSWORD;
 const DBUSER = process.env.DBUSER;
 const DBNAME = process.env.DBNAME;
@@ -11,10 +13,16 @@ const DBSERVERNAME = process.env.DBSERVERNAME;
 
 const EXPRESSPORT = 3000;
 
+// json middleware
+app.use(express.json());
+// server main pages
 app.use(express.static(
-    "public"
+    path.join(__dirname, "public")
 ));
+// custom routes for api requests
+app.use(require("./router"));
 
+// start server on port 3000
 app.listen(EXPRESSPORT, () => {
     console.log("listening on port " + EXPRESSPORT);
 });
