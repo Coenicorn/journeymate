4ccef171777d589db1f2d526e2f4621e8d1fbd9b
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-06 14:16:17 +0200

    async query function

--- src/createdb.js
+++ src/createdb.js
@@ -7,26 +7,33 @@ const DBNAME = process.env.DBNAME;
 const DBHOST = process.env.DBSERVERNAME;
 
 // connect to database
-const con = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    debug: true,
     host: DBHOST,
     user: DBUSER,
     password: DBPASSWORD
 });
 
+// async helper function for that sweet sweet async/await refactoring
 async function executeQuery(query) {
     return new Promise((resolve, reject) => {
-        
-    })
+        pool.getConnection((err, con) => {
+            if (err) reject(err);
+            else if (con)
+                con.query(query, (err, result)=>{
+                    if (err) reject(err);
+                    else resolve(result);
+                });
+            else reject("No connection");
+        });
+    });
 }
 
-con.connect(async (err) => {
-    if (err) throw err;
-    console.log("connected to db");
-
-    con.query("CREATE DATABASE userdata", (err, result) => {
-        if (err && err.code!= "ER_DB_CREATE_EXISTS") throw err;
-        console.log("database created");
-    });
+executeQuery("CREATE DATABASE userdata").catch((err) => {
+    // don't throw error if database creation fails; this is normal
+    // upon program restart if database has already been created
+    if (err.code != "ER_DB_CREATE_EXISTS") throw err;
 });
 
 module.exports = executeQuery;
