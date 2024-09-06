03968fe0d994146fd23bcb01fa817602fa83e4a2
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-06 17:41:45 +0200

    refactored initdb.js

--- src/initdb.js
+++ src/initdb.js
@@ -9,40 +9,22 @@ const DBNAME = process.env.DBNAME;
 const DBHOST = process.env.DBSERVERNAME;
 
 const con = mysql.createConnection({
-    host: DBHOST,
-    user: DBUSER,
-    password: DBPASSWORD
+  host: DBHOST,
+  user: DBUSER,
+  password: DBPASSWORD,
 });
 
-// try and create database
-// executeQuery(`CREATE DATABASE ${DBNAME}`).catch(err => {
-//     // don't throw error if database creation fails; this is normal
-//     // upon program restart if database has already been created
-//     if (err) throw err;
-// });
-con.query(`CREATE DATABASE ${DBNAME}`);
-
-// try and create table for userdata
-// line continuation ("\") so that the line is not interpreted with newline characters ("\n")
-// executeQuery(`CREATE TABLE userdata (\
-// id VARCHAR(36) NOT NULL PRIMARY KEY,\
-// name VARCHAR(30) NOT NULL,\
-// email VARCHAR(30) NOT NULL,\
-// password VARCHAR(50) NOT NULL,\
-// location_lat VARCHAR(50) NOT NULL,\
-// location_long VARCHAR(50) NOT NULL,\
-// reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-// )`).catch(err => {
-//     if (err) throw err;
-// });
-con.query(`USE journeymate`);
-
-con.query(`CREATE TABLE userdata (\
-id VARCHAR(36) NOT NULL PRIMARY KEY,\
-name VARCHAR(30) NOT NULL,\
-email VARCHAR(30) NOT NULL,\
-password VARCHAR(50) NOT NULL,\
-location_lat VARCHAR(50) NOT NULL,\
-location_long VARCHAR(50) NOT NULL,\
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)`);
+// try and create database and needed table
+con.query(`CREATE DATABASE ${DBNAME}`, () =>
+  con.query(`USE journeymate`, () =>
+    con.query(`CREATE TABLE userdata (\
+    id VARCHAR(36) NOT NULL PRIMARY KEY,\
+    name VARCHAR(30) NOT NULL,\
+    email VARCHAR(30) NOT NULL,\
+    password VARCHAR(50) NOT NULL,\
+    location_lat VARCHAR(50) NOT NULL,\
+    location_long VARCHAR(50) NOT NULL,\
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )`)
+  )
+);
