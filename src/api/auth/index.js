4829eb167e8eab9c2fde20de2e101a3b8cd4755d
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-09 11:11:13 +0200

    move auth logic

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -7,6 +7,21 @@ router.use("/", (request, response) => {
 });
 
 router.use("/signup", (request, response) => {
+    function fail(reason) {
+        response.status(400).send(`Invalid request: ${reason}`);
+    }
+   
+    // signup with username and password
+    const body = request.body;
+    
+    const email = body.email; if (!email) fail("No email provided");
+    const username = body.username; if (!username) fail("No username provided");
+    const password = body.password; if (!username) fail("No password provided");
+    const location_lat = body.location_lat; if (!location_lat) fail("Missing location data");
+    const location_long = body.location_long; if (!location_long) fail("Missing location data");
+    
+    executeQuery(`INSERT INTO userdata (id, email, password, name, location_lat, location_long) VALUE ('${uuid.v7()}', '${email}', '${password}', '${username}', '${location_lat}', '${location_long}')`);
+
 
 });
 
