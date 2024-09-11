27e4022c3bc2bfd4d5e365706aed43cc4daa5632
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-11 10:41:43 +0200

    route 404 to page

--- src/index.js
+++ src/index.js
@@ -24,7 +24,13 @@ app.use("/api", require("./api/"));
 app.use((req, res) => {
     res.status(404);
 
-    if (req.accepts(''))
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, "static"));
+    } else if (req.accepts("json")) {
+        res.json({ status: "404 not found" });
+    } else {
+        res.type("txt").send("404 not found");
+    }
 });
 
 // start server on port 3000
