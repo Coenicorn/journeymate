b52a669220287a0aa676d0dc97c5a6300e09060e
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-12 16:06:44 +0200

    work

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,6 @@
+const { validateSessionToken } = require("../authenticate");
+const { debuglog } = require("../util");
+
 const router = require("express").Router();
 
 // this file serves as the root for api calls under #/api/
@@ -7,7 +10,21 @@ const router = require("express").Router();
 router.use("/auth", require("./auth"));
 // validate auth header
 router.use(async (request, response) => {
-    
+    if (!request.headers.authorization) {
+        response.status(401).json({ status: "missing authentication headers" });
+        return;
+    }
+
+    const token = request.headers.authorization;
+    const tokenData = await validateSessionToken(token);
+
+    if (tokenData === 0) {
+        response.status(401).json({ status: "invalid session token", invalidToken: 1 });
+        debuglog(`token (${token}) failed authentication`);
+        return;
+    }
+
+    next();
 });
 router.use("/planner", require("./planner"));
 router.use("/game", require("./game"));
