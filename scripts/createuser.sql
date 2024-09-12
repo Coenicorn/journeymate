ec0d543b49eb7f4711dee6fe1f82de7aee4fe472
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-12 11:02:54 +0200

    update initdb scrpt

--- scripts/createuser.sql
+++ scripts/createuser.sql
@@ -1,5 +1,3 @@
-DROP USER 'journeymate'@'localhost';
-FLUSH PRIVILEGES;
 CREATE USER 'journeymate'@'localhost' IDENTIFIED BY '7bd665cd50c5bc5b03968c2adb73698c';
 GRANT SELECT, INSERT, UPDATE, CREATE, DELETE ON journeymate.* TO 'journeymate'@'localhost' IDENTIFIED BY '7bd665cd50c5bc5b03968c2adb73698c';
 FLUSH PRIVILEGES;
